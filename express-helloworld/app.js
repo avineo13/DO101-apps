eb991fb9285b13b00608a7ec674f4bdc86290c93
avineo13 <neo.enters.matrix@gmail.com>
2023-04-17 06:56:07 -0400

    add /mars endoint1

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -6,7 +6,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/mars', function (req, res) {
-  res.send('Follow the WHITE RABBIT!\n');
+  res.send('Follow the WHITE RABBIT...MATRIX!\n');
 });
 
 app.listen(8080, function () {
